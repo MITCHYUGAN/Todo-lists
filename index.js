67acde5ac84d1f411c8a2194136eb8c80baf2e2b
unknown <denmichigan123@gmai.com>
2022-07-08 13:22:08 +0100

    first to do list code

---
+++ index.js
@@ -0,0 +1,13 @@
+var tog = document.getElementById("taskter")
+document.getElementById("menu").addEventListener("click", function(){
+    document.tasker.classList.toggle()
+})
+
+const taskInput = document.querySelector(".task-input input")
+taskInput.addEventListener("keyup", e =>{
+    let userTask = taskInput.value;
+    if(e.key == "Enter"){
+        console
+    }
+
+})
